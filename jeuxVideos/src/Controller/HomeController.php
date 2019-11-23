74b93fcc68a7ffd2aabe4c582ce5b35e0de85b69
jeremy <jeremymarchal90@gmail.com>
2019-11-23 11:22:56 +0100

    first page

--- jeuxVideos/src/Controller/HomeController.php
+++ jeuxVideos/src/Controller/HomeController.php
@@ -1,12 +1,18 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
+    /**
+     * @return Response
+     * @Route("/", name="home")
+     */
     public function index(): Response
     {
-        return new Response('yolo');
+        return $this->render('pages/home.html.twig');
     }
 }
